a449006ae0e7203daf1f576e1b83f355876a487e
Tom Aldridge <tomaldridge16@icloud.com>
2024-02-21 17:08:31 +0000

    'more'

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!\n"
+def index():
+    images = os.listdir('static/imgs/')
+    return render_template('index.html', image_files=images)
+
+if __name__ == "__main__":
+    app.run()
